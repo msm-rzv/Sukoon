51d5145dd26a2c92fe72352d7e6b9bfacca7e9de
msmrzv <113569300+msmrzv@users.noreply.github.com>
2025-05-09 04:16:24 -0400

    hope javascrpit updates

---
+++ js/hope.js
@@ -0,0 +1,83 @@
+const ayahs = [
+    {
+      arabic: "قُلْ يَـٰعِبَادِىَ ٱلَّذِينَ أَسْرَفُوا۟ عَلَىٰٓ أَنفُسِهِمْ لَا تَقْنَطُوا۟ مِن رَّحْمَةِ ٱللَّهِ ۚ إِنَّ ٱللَّهَ يَغْفِرُ ٱلذُّنُوبَ جَمِيعًا ۚ إِنَّهُۥ هُوَ ٱلْغَفُورُ ٱلرَّحِيمُ ۝",
+      translation: "Say, 'O My servants who have transgressed against themselves [by sinning], do not despair of the mercy of Allah. Indeed, Allah forgives all sins. Indeed, it is He who is the Forgiving, the Merciful.' (39:53)",
+      tafsir: "...do not despair of the mercy of Allah: Qunut (despair) means to give up hope; and because this statement is addressed to the sinners with the intent of summoning them towards God, the meaning of rahma (mercy) here is the divine mercy that pertains specifically to the Hereafter rather than the more universal kind, which encompasses both this world and the Hereafter. And, obviously, the kind of mercy that the sinners will need with urgency in the Hereafter is forgiveness (maghfira). Therefore, the meaning of God's mercy in this verse is His forgiveness, which is why the next sentence gives the rationale behind this prohibition on losing hope as Indeed Allah will forgive all sins."
+    },
+    {
+      arabic: "يَـٰبَنِىَّ ٱذْهَبُوا۟ فَتَحَسَّسُوا۟ مِن يُوسُفَ وَأَخِيهِ وَلَا تَا۟يْـَٔسُوا۟ مِن رَّوْحِ ٱللَّهِ ۖ إِنَّهُۥ لَا يَا۟يْـَٔسُ مِن رَّوْحِ ٱللَّهِ إِلَّا ٱلْقَوْمُ ٱلْكَـٰفِرُونَ ۝",
+      translation: "'O my sons, go and find out about Joseph and his brother and despair not of relief from Allah. Indeed, no one despairs of relief from Allah except the disbelieving people.' (12:87)",
+      tafsir: "... and do not despair of Allah's mercy (rawḥ Allah)—the word rawḥ means breath or a pleasant breath. It is used figuratively to denote the state opposite to fatigue, which is relaxation or comfort. This is because severe hardships where hopes are cut off and paths to salvation are blocked can feel suffocating and oppressive to a person. Conversely, emerging into the vastness and openness of relief and attaining well-being feels like one can breathe again. Thus, the rawḥ attributed to God signifies relief after hardship by God's permission and will. Believers in God must believe that God does as He wills, and none can overturn His decree or judgment. One should never despair of the Spirit of God or lose hope in His mercy, as this would be tantamount to denying His omnipotence and disbelieving in His all-encompassing mercy. This sentiment is echoed in the present verse where Jacob says: Indeed no one despairs of Allah's mercy except the faithless lot, and the verse in which Abraham states: 'Who despairs of his Lord's mercy except those who are astray? (15:56). Despairing of the mercy of God is considered one of the most grievous sins a person can commit, according to the traditions. Therefore, the meaning of the verse is that Jacob bids his sons, 'Go, my sons, and look for Joseph and his brother who had been taken to Egypt in the hope of finding them. and do not despair of Allah's mercy’—meaning the relief that God provides after hardships-'Indeed no one despairs of Allah's mercy except the faithless lot this refers to those who do not believe that God has the power to alleviate all sorrows and to provide relief from all hardships. Power to alleviate all sorrows to..."
+    },
+    {
+      arabic: "قُل لَّن يُصِيبَنَآ إِلَّا مَا كَتَبَ ٱللَّهُ لَنَا هُوَ مَوْلَىٰنَا ۚ وَعَلَى ٱللَّهِ فَلْيَتَوَكَّلِ ٱلْمُؤْمِنُونَ ۝",
+      translation: "Say, 'Never will we be struck except by what Allah has decreed for us; He is our protector.' And upon Allah let the believers rely. (9:51)",
+      tafsir: "“In short, it is God alone who looks after our affairs—as shown by the statement, He is our patron, which is phrased in such a way as to indicate exclusivity and we have no say in the matter, nor any control over these apparent causes. On the contrary, the truth is that the affair rests entirely with God and He has afore ordained whatever good or evil and benefit or harm will reach us. This being the case, we should only focus on obeying His commands and striving to uphold His cause. It is God's will that determines whatever good or bad will befall us—God's servants must turn over their affairs to Him, and this is the meaning of trusting in God (tawakkul).”"
+    },
+    {
+      arabic: "لَا يُكَلِّفُ ٱللَّهُ نَفْسًا إِلَّا وُسْعَهَا ۚ لَهَا مَا كَسَبَتْ وَعَلَيْهَا مَا ٱكْتَسَبَتْ ۗ رَبَّنَا لَا تُؤَاخِذْنَآ إِن نَّسِينَآ أَوْ أَخْطَأْنَا ۚ رَبَّنَا وَلَا تَحْمِلْ عَلَيْنَآ إِصْرًۭا كَمَا حَمَلْتَهُۥ عَلَى ٱلَّذِينَ مِن قَبْلِنَا ۚ رَبَّنَا وَلَا تُحَمِّلْنَا مَا لَا طَاقَةَ لَنَا بِهِۦ ۖ وَٱعْفُ عَنَّا وَٱغْفِرْ لَنَا وَٱرْحَمْنَآ ۚ أَنتَ مَوْلَىٰنَا فَٱنصُرْنَا عَلَى ٱلْقَوْمِ ٱلْكَـٰفِرِينَ ۝",
+      translation: "Allah does not impose upon any soul more than what it can afford. All good will be for its own benefit, and all evil will be to its own loss. ˹The believers pray,˺ 'Our Lord! Do not punish us if we forget or make a mistake. Our Lord! Do not place a burden on us like the one you placed on those before us. Our Lord! Do not burden us with what we cannot bear. Pardon us, forgive us, and have mercy on us. You are our ˹only˺ Guardian. So grant us victory over the disbelieving people.' (2:286)",
+      tafsir: "The words Allah does not impose upon any soul more than what it can afford... describes the divinely established system that God does not impose upon His servants what is beyond their power; for example, He does not tell them to believe in that which is beyond their understanding; nor does He ordain for them a duty which they cannot do. This is, also, the custom of all sane persons. It is a sentence that is neither more nor less than the believers' declaration 'We hear and obey'; it agrees with it perfectly.This sentence Allah does not impose upon any soul... is in perfect harmony with the preceding and the following sentences. It is related to the preceding one, as it shows that God does not impose any duty except that which the servants can hear and obey, that is, which is within their ability and power."
+    },
+    {
+      arabic: "فَإِنَّ مَعَ ٱلْعُسْرِ يُسْرًا ۝ إِنَّ مَعَ ٱلْعُسْرِ يُسْرًۭا ۝",
+      translation: "'For indeed, with hardship [will be] ease. Indeed, with hardship [will be] ease.' (94:5–6)",
+      tafsir: "This is likely a justification and explanation for what was said previously about removing the Prophet's burden and elevating his name. The burden of the mission and the call to Islam–which are the heaviest burdens to bear for anyone–weighed heavily on the Prophet. Similarly, the people rejecting his call, disparaging him, and persisting to wipe out his name were burdensome on him. God eased that for him through inclining the people to accept his call; and elevated his name which they had sought to erase. This act of God was according to His ongoing custom of bringing ease after hardship. That is why in this verse He justifies His removal of the hardship from the Prophet by referring to His custom. And perhaps this can be matched with the law of impermanence: there is a constant flow of events, and everything is perpetually changing. For this reason, the definite article al- (the) in al-'usr [not translated in the English] indicates the genus of hardship in general (jins), not every hardship exhaustively (istighraq)."
+    },
+    {
+      arabic: "وَٱلَّذِينَ جَـٰهَدُوا۟ فِينَا لَنَهْدِيَنَّهُمْ سُبُلَنَا ۚ وَإِنَّ ٱللَّهَ لَمَعَ ٱلْمُحْسِنِينَ ۝",
+      translation: "'And those who strive for Us - We will surely guide them to Our ways. And indeed, Allah is with the doers of good.' (29:69)",
+      tafsir: "...strive in Us means that 'their struggle rests in Us.' This is a figura- tive way of saying that their efforts are expended in things connected to God, whether beliefs or actions. In other words, nothing will turn them aside from believing in Him, following His commands and observing His prohibitions. We shall surely guide them in Our ways...: God affirms the existence ways (subul) for Himself and whatever these may be, they all lead to Him, as a way (sabil) is nothing other than a means of reaching that to which it belongs, meaning its destination. Therefore, God's ways are those which bring people closer to Him and guide them towards Him. And insofar as the act of striving is itself a form of guidance, this means that guidance to God's ways is a kind of guidance upon guidance, as God alludes to in the verse: As for those who are guided, He enhances their guidance (47:17)."
+    },
+    {
+      arabic: "وَلَا تَهِنُوا۟ وَلَا تَحْزَنُوا۟ وَأَنتُمُ ٱلْأَعْلَوْنَ إِن كُنتُم مُّؤْمِنِينَ ۝",
+      translation: "'So do not weaken and do not grieve, and you will be superior if you are [true] believers.' (3:139)",
+      tafsir: "The promise, ‘you shall have the upper hand’, is general and unrestricted, but it is followed by the proviso, ‘if you are believers.’ It therefore gives the following meaning: You should not be weak in your will-power, nor should you grieve for the lost victory, if you are true believers. It is because belief is bound to give you an upper hand over your enemies–belief is accompanied by piety and patience, and these two are the basis of victory and triumph. As for the wound which has afflicted you in this battle, you are not alone in it; the unbelievers too had suffered similar casualties. If you think it over, they have not gained over you in any way. Therefore, you should not feel depressed or grieved."
+    },
+    {
+      arabic: "ٱلَّذِينَ قَالَ لَهُمُ ٱلنَّاسُ إِنَّ ٱلنَّاسَ قَدْ جَمَعُوا۟ لَكُمْ فَٱخْشَوْهُمْ فَزَادَهُمْ إِيمَـٰنًۭا وَقَالُوا۟ حَسْبُنَا ٱللَّهُ وَنِعْمَ ٱلْوَكِيلُ ۝",
+      translation: "'Those to whom hypocrites said, 'Indeed, the people have gathered against you, so fear them.' But it [merely] increased them in faith, and they said, 'Allah is sufficient for us and most excellent protector is he'' (3:173)",
+      tafsir: "... and they said: Allah is sufficient for us and most excellent Protector is (He): Hasbună (sufficient for us) is a word derived from al-hisab (to count, to reckon), because sufficiency is reckoned, vis-a-vis, the need. The sentence portrays their total dependence on God, because of their faith without caring about external causes which the divine system has created in the world. al-Wakil (agent, manager, representative) refers to one who manages the affairs on behalf of someone. The connotation of the verse is therefore similar to that of the verse 65:3: and whoever trusts in Allah, He is sufficient for him; surely Allah attains His purpose. That is why this clause (and they said: 'Allah is sufficient for us and most excellent Protector is [He]') has been followed by the words, So they returned with favor from Allah and (His) grace; no evil touched them. Therefore, when they followed His pleasure, He praised them in these words, and they followed the pleasure of Allah; and Allah is the Lord of mighty grace."
+    },
+    {
+      arabic: "وَأَيُّوبَ إِذْ نَادَىٰ رَبَّهُۥٓ أَنِّى مَسَّنِىَ ٱلضُّرُّ وَأَنتَ أَرْحَمُ ٱلرَّـٰحِمِينَ ۝ فَٱسْتَجَبْنَا لَهُۥ فَكَشَفْنَا مَا بِهِۦ مِن ضُرٍّۢ ۖ وَءَاتَيْنَـٰهُ أَهْلَهُۥ وَمِثْلَهُم مَّعَهُمْ رَحْمَةًۭ مِّنْ عِندِنَا وَذِكْرَىٰ لِلْعَـٰبِدِينَ ۝",
+      translation: "And ˹remember˺ when Job cried out to his Lord, 'I have been touched with adversity, and You are the Most Merciful of the merciful.' So We answered his prayer and removed his adversity, and gave him back his family, twice as many, as a mercy from Us and a lesson for the ˹devoted˺ worshippers. (21:83–84)",
+      tafsir: "Job was enveloped by calamity. He lost his property, his children died, and he was afflicted in his body with a severe illness for a protracted period. He then called to God and complained to Him about his condition. Thus God answered his prayer, delivered him from his illness, and returned his property and children to him—along with the like of them, as He says in the subsequent verse."
+    },
+    {
+      arabic: "فَمَن يَعْمَلْ مِنَ ٱلصَّـٰلِحَـٰتِ وَهُوَ مُؤْمِنٌۭ فَلَا كُفْرَانَ لِسَعْيِهِۦ وَإِنَّا لَهُۥ كَـٰتِبُونَ ۝",
+      translation: "'So whoever does good and is a believer will never be denied ˹the reward for˺ their striving, for We are recording it all.' (21:94)",
+      tafsir: "there shall be no denying (kufrāna) of his exertion: That is: His goods deeds will not be covered, where kufran is the opposite of gratitude (shukr), as the same idea is mentioned elsewhere as follows: And your exertion shall be thanked (76:22). and surely We will write (it) down for him: That is: 'We will write it in the scrolls of deeds, a writing that shall not be forgotten.' Thus, there shall be no denying of his exertion, and surely We will write (it) down for him means that his good deed will neither be forgotten nor left unappreciated. This is one of the verses which indicate that the acceptance of good deeds is conditional upon belief, which is confirmed by verses that indicate the futility of [good] deeds with disbelief. It also indicates that a believer who does some good deeds is among those who will be saved."
+    }
+  ];
+
+  let currentAyah = null;
+
+  window.onload = function () {
+    loadRandomAyah();
+  };
+
+  function loadRandomAyah() {
+    currentAyah = ayahs[Math.floor(Math.random() * ayahs.length)];
+    document.getElementById('arabic').innerText = currentAyah.arabic;
+    document.getElementById('translation').innerText = currentAyah.translation;
+  }
+  
+  function openTafsir() {
+    if (currentAyah && currentAyah.tafsir) {
+      document.getElementById('tafsir-text').innerText = currentAyah.tafsir;
+      document.getElementById('tafsir-modal').style.display = 'block';
+    }
+  }
+  
+  function closeTafsir() {
+    document.getElementById('tafsir-modal').style.display = 'none';
+  }
+  
+  window.onclick = function(event) {
+    const modal = document.getElementById('tafsir-modal');
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
