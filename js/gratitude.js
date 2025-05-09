b633df0653abf1323b1ccb228498ed5e0637f8ad
msmrzv <113569300+msmrzv@users.noreply.github.com>
2025-05-09 03:44:38 -0400

    gratitude verses and fixes

---
+++ js/gratitude.js
@@ -0,0 +1,82 @@
+const ayahs = [
+    {
+      arabic: "وَإِذْ تَأَذَّنَ رَبُّكُمْ لَئِن شَكَرْتُمْ لَأَزِيدَنَّكُمْ ۖ وَلَئِن كَفَرْتُمْ إِنَّ عَذَابِى لَشَدِيدٌۭ ۝",
+      translation: "And [remember] when your Lord proclaimed: 'If you are grateful, I will surely increase you [in favor]; but if you deny, indeed, My punishment is severe.' (14:7)",
+      tafsir: "This declaration, according to the narrative, serves as a boon for the grateful. For the ungrateful, however, it stands as a warning and a sign of loss. It aligns with previous discussions, devoid of specific limitations or exclusions. Evidently, this declaration operates independently. God elucidates the essence of gratitude, implying the appropriate utilization of blessings in a manner that recalls the giver's generosity. Gratitude results in augmented blessings, whereas ingratitude leads to severe consequences. This is reflected, for example, in the story of Noah, where he turns to God, pleading with his community: 'Plead to your Lord for forgiveness. Indeed, He is all-forgiving. He will send for you abundant rains from the sky, and aid you with wealth and sons, and provide you with gardens and provide you with streams' (Nūḥ:10-12)."
+    },
+    {
+      arabic: "فَٱذْكُرُونِىٓ أَذْكُرْكُمْ وَٱشْكُرُوا۟ لِى وَلَا تَكْفُرُونِ ۝",
+      translation: "So remember Me; I will remember you. And be grateful to Me and do not deny Me. (2:152)",
+      tafsir: "He (Allah) calls them (believers) to remember Him and to be thankful to Him. They should remember Him with their devotion and obedience, so that He remembers them with His favor. He will increase for them His bounty and grace, if they are thankful to Him, and if they are not ungrateful for His favors. He had already said to them: and remember your Lord when you forget and say: May be my Lord will guide me to a nearer course to the right than this (18:24); If you are grateful, I would certainly give you more; and if you are ungrateful, My chastisement is truly severe (14:7). These two verses were revealed before the verse of the qiblah in this chapter. al-Dhikr (remembrance) is sometimes used as opposite of al-ghaflah (inattention and heedlessness). God says: and do not follow him whose heart We have made inattentive to Our remembrance (18:28). Inattention is 'not knowing what one knows—although the original knowledge is present in memory.' 'Remembrance,' as its opposite, means 'knowing what one knows.' The word 'remembrance' at other times is used as opposite to al-nisyan (forgetfulness). Forgetfulness is 'cessation of knowledge from the mind'; and as its opposite, remembrance means 'presence of knowledge in the mind.' It is to this meaning that the words of God refer in the verse: and remember your Lord when you forget... (18:24). This remembrance, like its opposite 'forgetfulness,' is a concept which has its own special effects and exclusive characteristics. Sometimes when those effects and characteristics are found, we say that one remembers, although there is no actual remembrance; or that one has forgotten, although there is no actual forgetfulness. For example, if you do not come to the aid of your friend—when you know that he needs your help—then people say that: 'You have forgotten him,' although in fact you remember him very well."
+    },
+    {
+      arabic: "وَلَقَدْ ءَاتَيْنَا لُقْمَـٰنَ ٱلْحِكْمَةَ أَنِ ٱشْكُرْ لِلَّهِ ۚ وَمَن يَشْكُرْ فَإِنَّمَا يَشْكُرُ لِنَفْسِهِۦ ۖ وَمَن كَفَرَ فَإِنَّ ٱللَّهَ غَنِىٌّ حَمِيدٌۭ ۝",
+      translation: "And certainly we gave Luqman wisdom to be grateful to Allah. And whoever is grateful, is only grateful for [the benefit of] himself. And whoever is ungrateful then indeed, Allah is indeed all-sufficient, all-laudable. (31:12)",
+      tafsir: "And whoever gives thanks, gives thanks only for his own sake. And whoever is ungrateful, Allah is indeed all-sufficient, all-laudable: Here, God says He does not need people to give Him thanks, because the benefits of the act of thanksgiving actually return to the person giving thanks. Equally, ingratitude only harms the one who is ungrateful—it does not harm God in the slightest. Therefore, whoever gives thanks only does so for his own benefit, as God does not benefit from it—because He is absolutely self-sufficient (ghani)—for the same reason, whoever is ungrateful only harms himself. In other words, God derives no benefit or harm whether people give thanks or not. He is All-laudable (hamid) because he is praised for the blessings He gives, regardless of how people receive them. The Qur'an uses the present tense for thanksgiving to show that it is something continuous and the past tense for ingratitude to show that it happens on occasion. This implies that thanksgiving is only of benefit if it is continuous but that ingratitude is harmful even if it only happens once."
+    },
+    {
+      arabic: "إِنَّا هَدَيْنَـٰهُ ٱلسَّبِيلَ إِمَّا شَاكِرًۭا وَإِمَّا كَفُورًۭا ۝",
+      translation: "Indeed, We guided him to the way, be he grateful or be he ungrateful. (76:3)",
+      tafsir: "What is meant by hidayah (guidance) in this verse is 'showing the way to someone,' not taking them to the destination. Sabil is meant in its literal sense, which is 'a way that leads to the desired goal,' which would be the path of truth. One is grateful for a bounty if they use it in a way that demonstrates whom it is from. One is ungrateful if they use a blessing while denying and concealing whom it is from. As we explained under the verse: and soon Allah will reward the grateful (3:144), the reality of gratefulness is sincere servitude toward God. ... be he (immā) grateful or be he (immā) ungrateful: Some exegetes have said that this final phrase is a description of the way. However, it is a description for the pronoun 'him' in the clause, We have shown him. The word imma (whether, either this or that, be this or that) is a partitive operator. The meaning is: 'We have shown them the way, while they are divided into either being grateful or ungrateful.' In other words: 'They are shown the way, regardless of whether they fall into this category or the other.'"
+    },
+    {
+      arabic: "فَكُلُوا۟ مِمَّا رَزَقَكُمُ ٱللَّهُ حَلَـٰلًۭا طَيِّبًۭا وَٱشْكُرُوا۟ نِعْمَتَ ٱللَّهِ إِن كُنتُمْ إِيَّاهُ تَعْبُدُونَ ۝",
+      translation: "So eat of what Allah has provided for you [which is] lawful and good. And be grateful for the favor of Allah, if it is [indeed] Him that you worship. (16:114)",
+      tafsir: "This sets the context for the following instruction: So eat out of what Allah has provided you as lawful and good ..., derived as a conclusion from the parable. Since experiencing such punishment results from ingratitude for abundant sustenance and denying the call to righteousness, people are instructed to eat what God has provided, as long as it is lawful and good, and to be thankful to God if they truly worship Him."
+    },
+    {
+      arabic: "رَبِّ أَوْزِعْنِىٓ أَنْ أَشْكُرَ نِعْمَتَكَ ٱلَّتِىٓ أَنْعَمْتَ عَلَىَّ وَعَلَىٰ وَٰلِدَىَّ وَأَنْ أَعْمَلَ صَـٰلِحًۭا تَرْضَىٰهُ وَأَدْخِلْنِى بِرَحْمَتِكَ فِى عِبَادِكَ ٱلصَّـٰلِحِينَ ۝",
+      translation: "So [Solomon] smiled, amused at her speech, and said, 'My Lord, enable me to be grateful for Your favor which You have bestowed upon me and upon my parents with, and to do good deeds that please you. Admit me, by Your mercy, into ˹the company of˺ Your righteous servants.”' (27:19)",
+      tafsir: "In asking for gratefulness, he (Solomon) specifies God's favors upon himself and upon his parents. This is because the blessings upon his parents were in a way blessings upon himself, for his existence was through them. God blessed his father David with prophethood, kingdom, wisdom, decisive judgment and other favors; and He blessed his mother by making her the wife of the Prophet David, giving her Prophet Solomon as a son. Thus God made her part of the household of prophethood. This supplication shows that Solomon's mother was among those who followed the straight path. She was one of those whom God has blessed, who are one of the four groups mentioned in the verse: Those upon whom Allah has bestowed favors, of the prophets and the truthful and the martyrs and the righteous (4:69). and that I should act righteously such as Thou art pleased with.... The conjunction 'and' joins this sentence to: that I should be grateful for Thy favor. It means: 'Inspire me to do righteously....' This request is much loftier and nobler than asking for tawfiq (facilitation, ability, reconciling) to do good deeds, because facilitation pertains to the availability of external means in a way that matches one's felicity, while the inspiration that he asked for is an inner drive toward felicity and salvation. Thus, it is likely that he intended was to be given the same kind of wahy (revelation, inspiration) with which God had honored Abraham and his family. God describes this wahy where He says: And We revealed to them the doing of good (21:73). Essentially, this kind of wahy refers to being aided by the Holy Spirit, as we discussed in our exposition of verse 21:73."
+    },
+    {
+      arabic: "وَٱللَّهُ أَخْرَجَكُم مِّنۢ بُطُونِ أُمَّهَـٰتِكُمْ لَا تَعْلَمُونَ شَيْـًۭٔا وَجَعَلَ لَكُمُ ٱلسَّمْعَ وَٱلْأَبْصَـٰرَ وَٱلْأَفْـِٔدَةَ ۙ لَعَلَّكُمْ تَشْكُرُونَ ۝",
+      translation: "And Allah has extracted you from the wombs of your mothers not knowing a thing, and He made for you hearing and vision and intellect that perhaps you would be grateful. (16:78)",
+      tafsir: "Regarding the verse, Allah has brought you forth from the bellies of your mothers while you did not know anything, it negates the presence of acquired knowledge in humans at birth. The phrase, Allah has brought you forth from the bellies of your mothers, refers to birth, and while you did not know anything, is a condition of the pronoun ‘your’, meaning you were born from your mothers' wombs without the knowledge which you later acquired through senses, imagination, and intellect. This verse supports the view of psychologists that the human psyche is initially like a blank slate, which gradually acquires imprints over time. However, this pertains to human knowledge about things other than themselves. Mystics do not consider self-awareness as 'knowing something', supported by the Qur'anic statement in previous verses about those reaching the nethermost age, so that he knows nothing after some knowledge (16:70). It is evident that such individuals still possess self-awareness; aging does not lead to self-ignorance."
+    },
+    {
+      arabic: "وَءَاتَىٰكُم مِّن كُلِّ مَا سَأَلْتُمُوهُ ۚ وَإِن تَعُدُّوا۟ نِعْمَتَ ٱللَّهِ لَا تُحْصُوهَآ ۗ إِنَّ ٱلْإِنسَـٰنَ لَظَلُومٌۭ كَفَّارٌۭ ۝",
+      translation: "And He gave you from all you asked of Him. And if you should count the favors of Allah, you could not enumerate them. Indeed, mankind is [generally] most unjust and ungrateful. (14:34)",
+      tafsir: "Regarding the words, 'If you enumerate Allah's blessings, you will not be able to count them,' al-Raghib says: The term 'enumeration' (al-ihsa) involves counting numerically. This term derives from the word for pebbles (basa). This connection can be traced back to the ancient practice where pebbles served as counting tools, akin to how we employ our fingers in the present day. In summary, the text suggests that the blessings bestowed by God transcend the scope of human calculation. It is inherently impossible for humans to enumerate the myriad needs that God has fulfilled through His blessings. And how can one possibly count the blessings of the Almighty when the entire universe, in all its parts, along with its various attributes and states, is intricately interconnected? Each aspect is beneficial to another; and they are all interdependent. In this context, everything is a blessing in relation to the whole, a reality that eludes any attempt at comprehensive enumeration."
+    },
+    {
+      arabic: "وَهُوَ ٱلَّذِىٓ أَنشَأَ لَكُمُ ٱلسَّمْعَ وَٱلْأَبْصَـٰرَ وَٱلْأَفْـِٔدَةَ ۚ قَلِيلًۭا مَّا تَشْكُرُونَ ۝",
+      translation: "He is the One Who created for you hearing, sight, and intellect. ‘Yet’ you hardly give any thanks. (23:78)",
+      tafsir: "God starts describing His blessings upon the audience by referring to ears and eyes, two organs that are unique to animals. The creation of these two organs in animals is original, unprecedented, and not based on anything similar in the simpler forms of being, such as plants, objects and elements. Given these two senses, the creatures that are equipped with them find themselves at a new plane of existence with an immeasurably wider scope of activities compared to creatures that lack these two faculties. These creatures can understand what is good for them and what is bad, what is beneficial and what is harmful. Then they can perform voluntary movements toward what they like, and away from what they dislike. Therefore, they live in a new realm of existence with certain manifestations of beauty, pleasure, might, dominance and love, which were nowhere to be found at more primitive planes of existence. It has been said that the verse only mentions hearing and sight out of all senses because these two are necessary and sufficient in completing the proof. Then He mentions the 'heart' which refers to the faculty of intellection and understanding in humankind. This is a bounty that is specifically given to humankind out of all animals."
+    },
+    {
+      arabic: "وَهُوَ ٱلَّذِى جَعَلَ ٱلَّيْلَ وَٱلنَّهَارَ خِلْفَةًۭ لِّمَنْ أَرَادَ أَن يَذَّكَّرَ أَوْ أَرَادَ شُكُورًۭا ۝",
+      translation: "And it is He Who made the Night and the Day to follow each other (khilfah) : for him who desires to remember or desires to be thankful. (25:62)",
+      tafsir: "The verse implies God's domination and/or His grace over His servants, by creating the night and the day in such a way that each of them replaces the other. Thus, if one fails to be a believer in one period of time, he can make up for it in the other, and if one misses an act of worship or any righteous deed in either of these two times, he can do it in the other. The above interpretation is what we can infer from the verse, which is also relevant to the previous verse, where it says: and made therein a lamp and a shining moon (25:61). It means that even though God has blocked the arrogant disbelievers from rising to His Precinct of Proximity, He has not prevented His servants from seeking proximity to Him and benefiting from His light. He has illuminated the day by a bright sun, has illuminated the night by a shining moon, and has made the two follow one another. Thus, if one misses to remember Him or thank Him in one of them, he can do so in the other."
+    }
+  ];
+  
+  let currentAyah = null;
+
+  window.onload = function () {
+    loadRandomAyah();
+  };
+
+  function loadRandomAyah() {
+    currentAyah = ayahs[Math.floor(Math.random() * ayahs.length)];
+    document.getElementById('arabic').innerText = currentAyah.arabic;
+    document.getElementById('translation').innerText = currentAyah.translation;
+  }
+  
+  function openTafsir() {
+    if (currentAyah && currentAyah.tafsir) {
+      document.getElementById('tafsir-text').innerText = currentAyah.tafsir;
+      document.getElementById('tafsir-modal').style.display = 'block';
+    }
+  }
+  
+  function closeTafsir() {
+    document.getElementById('tafsir-modal').style.display = 'none';
+  }
+  
+  window.onclick = function(event) {
+    const modal = document.getElementById('tafsir-modal');
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
